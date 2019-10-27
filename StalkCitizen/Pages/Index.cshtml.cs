9cb415192f47f07e6d45d80d32abe032c288e64a
Sławomir Rosiek <rosieks@hotmail.com>
2019-10-27 14:39:40 +0100

    Added some additional fields from CPR

--- StalkCitizen/Pages/Index.cshtml.cs
+++ StalkCitizen/Pages/Index.cshtml.cs
@@ -23,9 +23,21 @@ namespace StalkCitizen.Pages
         {
             return Task.FromResult(new CitizenData
             {
-                CprNumber = cpr,
+                Cpr = cpr,
                 FirstName = "Kevin",
-                LastName = "Magnussen"
+                LastName = "Magnussen",
+                MaritalStatus = "married",
+                Addresses = new List<Address>
+                {
+                    new Address
+                    {
+                        City = "Roskilde",
+                        MunicipalityName = "Roskilde",
+                        PostCode = "11111",
+                        
+                    },
+                },
+                Citizenships = new [] { "DK" },
             });
         }
     }
@@ -37,10 +49,43 @@ namespace StalkCitizen.Pages
 
     public class CitizenData
     {
-        public string CprNumber { get; set; }
+        public string Cpr { get; set; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public string MaritalStatus { get; set; }
+
+        public IReadOnlyList<Address> Addresses { get; set; }
+
+        public IReadOnlyList<string> Citizenships { get; set; }
+    }
+
+    public class Address
+    {
+        public string BuildingNo { get; set; }
+
+        public string City { get; set; }
+
+        public string MunicipalityCode { get; set; }
+
+        public string MunicipalityName { get; set; }
+
+        public string RoadCode { get; set; }
+
+        public string DarAddress { get; set; }
+
+        public string Floor { get; set; }
+
+        public string HouseNo { get; set; }
+
+        public string PostDistrict { get; set; }
+
+        public string PostCode { get; set; }
+
+        public string DoorNo { get; set; }
+
+        public string RoadAddressingName { get; set; }
     }
 }
