59fcd1a0f0d431cc737a37cbd5e00fe74196718d
Konrad Banaszek <WKO@kmd.dk>
2019-10-30 09:49:21 +0100

    config fixed

--- StalkCitizen/StalkCitizenConfiguration.cs
+++ StalkCitizen/StalkCitizenConfiguration.cs
@@ -12,15 +12,24 @@ namespace StalkCitizen
         
         public DigitalPostOptions DigitalPost { get; set; } 
 
+        public AuthenticationOptions Authentication { get; set; }
+
         public string AllowedHosts { get; set; } = string.Empty;
+
         public string SerilogAzureEventHubEventSource { get; set; }
+
         public string SerilogAzureEventHubConnectionString { get; set; }
-        public string ClientId { get; set; }
     }
 
     class DigitalPostOptions
     {
         public Guid SubscriptionId { get; set; }
         public Guid DigitalPostConfigurationId { get; set; }
+    }
+
+    class AuthenticationOptions
+    {
+        public string Authority { get; set; }
+        public string ClientId { get; set; }
     }
 }
